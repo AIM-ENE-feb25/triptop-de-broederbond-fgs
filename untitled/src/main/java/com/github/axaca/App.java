344d260a25593d781de87a95d53b70918d191b60
Axaca <150051152+Axaca@users.noreply.github.com>
2025-03-18 11:32:55 +0100

    RapidAPI POC
    Signed-off-by: Axaca <150051152+Axaca@users.noreply.github.com>

---
+++ untitled/src/main/java/com/github/axaca/App.java
@@ -0,0 +1,18 @@
+package com.github.axaca;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+
+public class App 
+{
+    public static void main(String[] args) throws UnirestException {
+        HttpResponse<String> response = Unirest.get("https://real-time-tripadvisor-scraper-api.p.rapidapi.com/tripadvisor_restaurants_search_v2?location=new%20york")
+                .header("x-rapidapi-host", "real-time-tripadvisor-scraper-api.p.rapidapi.com")
+                .header("x-rapidapi-key","5f812d74c8msh929edfd3b340811p1a0dcajsn3f7bc78f747b")
+                .asString();
+
+        System.out.println(response.getBody());
+    }
+
+}
