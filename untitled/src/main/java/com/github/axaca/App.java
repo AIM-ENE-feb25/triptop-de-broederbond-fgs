09931ba808d4f179be7df907f23a9b75008d8028
Axaca <150051152+Axaca@users.noreply.github.com>
2025-03-21 11:51:10 +0100

    Updates
    Signed-off-by: Axaca <150051152+Axaca@users.noreply.github.com>

--- untitled/src/main/java/com/github/axaca/App.java
+++ untitled/src/main/java/com/github/axaca/App.java
@@ -7,7 +7,7 @@ import com.mashape.unirest.http.exceptions.UnirestException;
 public class App 
 {
     public static void main(String[] args) throws UnirestException {
-        HttpResponse<String> response = Unirest.get("https://real-time-tripadvisor-scraper-api.p.rapidapi.com/tripadvisor_restaurants_search_v2?location=new%20york")
+        HttpResponse<String> response = Unirest.get("https://real-time-tripadvisor-scraper-api.p.rapidapi.com/tripadvisor_tours_search_v2?location=new%20york")
                 .header("x-rapidapi-host", "real-time-tripadvisor-scraper-api.p.rapidapi.com")
                 .header("x-rapidapi-key","5f812d74c8msh929edfd3b340811p1a0dcajsn3f7bc78f747b")
                 .asString();
