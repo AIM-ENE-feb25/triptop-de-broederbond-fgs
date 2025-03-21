e6c0c44c211a99ceffbd1b5c3078218f8791fb0b
Felix Koster <FALM.koster@student.han.nl>
2025-03-21 10:43:28 +0100

    demo en prototype booking api

--- uitwerkingen/demo_bookingcom/src/main/java/org/example/App.java
+++ uitwerkingen/demo_bookingcom/src/main/java/org/example/App.java
@@ -1,15 +1,27 @@
 package org.example;
 
 import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import org.json.JSONObject;
 
 public class App 
 {
     public static void main( String[] args ) throws UnirestException {
-        HttpResponse<String> response = Unirest.get("https://booking-com.p.rapidapi.com/v1/hotels/locations?name=Nijmegen&locale=en-gb")
-                .header("X-RapidAPI-Key", "cbb15196e0mshb44c607aede1353p17d241jsnd8ce2f2aed4d")
-                .header("X-RapidAPI-Host", "booking-com.p.rapidapi.com").asString();
-        System.out.println(response.getBody());
+//        HttpResponse<String> response = Unirest.get("https://booking-com.p.rapidapi.com/v1/hotels/locations?name=Nijmegen&locale=en-gb")
+//                .header("X-RapidAPI-Key", "cbb15196e0mshb44c607aede1353p17d241jsnd8ce2f2aed4d")
+//                .header("X-RapidAPI-Host", "booking-com.p.rapidapi.com").asString();
+//        System.out.println(response.getBody());
+
+        String url = "https://triptop-identity.wiremockapi.cloud/login";
+        HttpResponse<JsonNode> loginResponse = Unirest.post(url)
+                .header("Content-Type", "application/json")
+                .body(new JSONObject().put("username", "edevries").put("password", "3g2Rw9sT1x"))
+                .asJson();
+        System.out.println(loginResponse.getBody());
+
+        String url1 = "https://triptop-identity.wiremockapi.cloud/checkAppAccess?token="
+        HttpResponse<JsonNode> roleResponse = Unirest.post()
     }
 }
---
+++ uitwerkingen/demo_bookingcom/src/main/java/org/example/User.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class User {
+
+    private String username;
+    private String password;
+    private String voornaam;
+    private String achternaam;
+
+    public User(String username, String password, String voornaam, String achternaam) {
+        this.username = username;
+        this.password = password;
+        this.voornaam = voornaam;
+        this.achternaam = achternaam;
+    }
+}
