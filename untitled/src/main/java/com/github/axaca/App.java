684ca214029b48de0bad5adc35ee04b8c1dfbb53
Axaca <150051152+Axaca@users.noreply.github.com>
2025-03-28 13:08:23 +0100

    dingen
    Signed-off-by: Axaca <150051152+Axaca@users.noreply.github.com>

--- untitled/src/main/java/com/github/axaca/App.java
+++ untitled/src/main/java/com/github/axaca/App.java
@@ -6,13 +6,14 @@ import com.mashape.unirest.http.exceptions.UnirestException;
 
 public class App 
 {
-    public static void main(String[] args) throws UnirestException {
-        HttpResponse<String> response = Unirest.get("https://real-time-tripadvisor-scraper-api.p.rapidapi.com/tripadvisor_tours_search_v2?location=new%20york")
-                .header("x-rapidapi-host", "real-time-tripadvisor-scraper-api.p.rapidapi.com")
-                .header("x-rapidapi-key","5f812d74c8msh929edfd3b340811p1a0dcajsn3f7bc78f747b")
-                .asString();
 
-        System.out.println(response.getBody());
+    public static void main(String[] args) throws UnirestException {
+//        HttpResponse<String> response = Unirest.get("https://real-time-tripadvisor-scraper-api.p.rapidapi.com/tripadvisor_tours_search_v2?location=new%20york")
+//                .header("x-rapidapi-host", "real-time-tripadvisor-scraper-api.p.rapidapi.com")
+//                .header("x-rapidapi-key","5f812d74c8msh929edfd3b340811p1a0dcajsn3f7bc78f747b")
+//                .asString();
+//
+//        System.out.println(response.getBody());
     }
 
 }
